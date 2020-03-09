f968c4088b3f53c406457135f6af3f40b294303d
OptimusOrange <61796068+OptimusOrange@users.noreply.github.com>
2020-03-09 11:31:29 -0400

    Add files via upload

---
+++ main.js
@@ -0,0 +1,33 @@
+let app = new Vue({
+  el: "#app",
+  data: {
+      title: 'TODOS',
+      todos: [
+        { text: 'option 1', done: false, id: Date.now() },
+        { text: 'option 2', done: false, id: Date.now() + 1 }
+      ]
+    },
+  watch:{
+    todos: function(NEWtodos) {
+      localStorage.setItem('todo' , JSON.stringify(this.todos))
+
+    }
+  },
+  methods: {
+    addTodo(event) {
+      const text = event.target.value
+      this.todos.push({text, done: false, id: Date.now()})
+      event.target.value = ''
+    },
+    removeTodo(id) {
+      this.todos = this.todos.filter(todo => todo.id !== id)
+    },      
+    check(todo){
+      todo.done = !todo.done
+      localStorage.setItem('todo' , JSON.stringify(this.todos))
+    }
+  },
+  mounted() {
+  this.todos=JSON.parse(window.localStorage.getItem('todo'))
+  },
+});
